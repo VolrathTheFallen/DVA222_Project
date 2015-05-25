5659ae92396738f5fc4c6fb398b756f8d8c68c78
p0pup <filip.lagerholm@outlook.com>
2015-05-25 09:15:43 -0700

    Added titel to groups

---
+++ DVA222_Project/ContainerUI.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "ContainerBase.h"
+#include "ContainerUI.h"
+
+ContainerUI::ContainerUI()
+{
+
+}
+
+ContainerUI::ContainerUI(int posX, int posY, int width, int height, Color c, string titel)
+	:ContainerBase(posX,posY,width,height,c)
+{
+	this->titel = titel;
+}

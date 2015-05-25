8adf745ea44cad87f2de653b589fa3fbb13092f4
Erik Liljeqvist <blastoderm@msn.com>
2015-05-25 08:35:05 +0200

    MyPanel Done

--- DVA222_Project/MyPanel.cpp
+++ DVA222_Project/MyPanel.cpp
@@ -1,1 +1,22 @@
 #include "stdafx.h"
+#include "ContainerBase.h"
+#include "MyPanel.h"
+
+
+MyPanel::MyPanel()
+{
+
+}
+
+MyPanel::MyPanel(int posX, int posY, int width, int height, Color color)
+	:ContainerBase(posX, posY, width, height, color)
+{
+
+}
+
+void MyPanel::OnPaint()
+{
+	glColor3f(color.R / 255.0, color.G / 255.0, color.B / 255.0);
+
+	FillRectangle(X + relativePos.X, Y + relativePos.Y, Width, Height);
+}
--- DVA222_Project/MyPanel.h
+++ DVA222_Project/MyPanel.h
@@ -5,5 +5,10 @@
 
 class MyPanel : public ContainerBase
 {
+public:
 
+	MyPanel();
+	MyPanel(int posX, int posY, int width, int height, Color color);
+
+	void OnPaint();
 };
